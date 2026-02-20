f0e926168759038fc8390e58d26b48c1dd48e983
RenukaMadhu <renuka.cseds25@cmrit.ac.in>
2026-02-20 15:56:04 +0530

    Add files via upload

---
+++ #Band name generator.py
@@ -0,0 +1,9 @@
+#band name generator
+print("Welcome to the Band Name Generator 🎸")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+band_name = city + " " + pet
+
+print("Your band name could be:", band_name)
